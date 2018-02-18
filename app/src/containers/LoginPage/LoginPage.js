87b0bf66337343ff53b8c4364fef1fd48ff3c8c4
Felicia Ringblom <felicia.ringblom@gmail.com>
2018-02-18 12:22:18 +0100

    Fix linting errors

--- app/src/containers/LoginPage/LoginPage.js
+++ app/src/containers/LoginPage/LoginPage.js
@@ -15,15 +15,15 @@ class LoginPage extends Component {
     }
 
     login() {
-        var client_id = 'eff83e6b5dfe489092cde0ec5f788a4c'; // Your client id
-        var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+        const client_id = 'eff83e6b5dfe489092cde0ec5f788a4c'; // Your client id
+        const redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
 
-        var state = this.generateRandomString(16);
+        const state = this.generateRandomString(16);
 
         localStorage.setItem(stateKey, state);
-        var scope = 'user-read-private user-read-email';
+        const scope = 'user-read-private user-read-email';
 
-        var url = 'https://accounts.spotify.com/authorize';
+        let url = 'https://accounts.spotify.com/authorize';
         url += '?response_type=token';
         url += '&client_id=' + encodeURIComponent(client_id);
         url += '&scope=' + encodeURIComponent(scope);
@@ -34,11 +34,11 @@ class LoginPage extends Component {
     };
 
     generateRandomString(length) {
-        var text = '';
-        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-        for (var i = 0; i < length; i++) {
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        for (let i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
         return text;
     };
