f84f2a9147a4f222878ff69de83f7e05e4a46c78
Felicia Ringblom <felicia.ringblom@gmail.com>
2018-05-13 22:05:44 +0200

    Fix linting errors

--- app/src/actions/websocketActions.js
+++ app/src/actions/websocketActions.js
@@ -1,19 +1,19 @@
 import * as actionTypes from './actionTypes';
 
-export const createGame = (quiz) =>  {
-  return (dispatch, getState, {emit}) => {
-    emit(actionTypes.WS_CREATE_GAME, quiz)
-  }
-}
+export const createGame = (quiz) => {
+  return (dispatch, getState, { emit }) => {
+    emit(actionTypes.WS_CREATE_GAME, quiz);
+  };
+};
 
-export const joinGame = (id, userName) =>  {
-  return (dispatch, getState, {emit}) => {
-    emit(actionTypes.WS_JOIN_GAME, {id: id, userName: userName})
-  }
-}
+export const joinGame = (id, userName) => {
+  return (dispatch, getState, { emit }) => {
+    emit(actionTypes.WS_JOIN_GAME, { id: id, userName: userName });
+  };
+};
 
-export const launchGame = (id) =>  {
-  return (dispatch, getState, {emit}) => {
-    emit(actionTypes.WS_LAUNCH_GAME, {id: id})
-  }
-}
+export const launchGame = (id) => {
+  return (dispatch, getState, { emit }) => {
+    emit(actionTypes.WS_LAUNCH_GAME, { id: id });
+  };
+};
