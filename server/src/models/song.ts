c596d18d340ca2bca72964fa2e8eef8edbf577e2
Oscar West <oscar.west@collectorbank.se>
2018-06-10 21:03:03 +0200

    Add preview_url from tracks request in songs model

--- server/src/models/song.ts
+++ server/src/models/song.ts
@@ -5,6 +5,7 @@ export class Song {
   id = '';
   title : string;
   artist : string;
+  previewUrl : string;
 
   constructor(data: Song | {} = {}) {
     Object.assign(this, data);
--- server/src/services/spotifyService.ts
+++ server/src/services/spotifyService.ts
@@ -20,7 +20,7 @@ export class SpotifyService {
   public async getSongs(userId: string, playlistId: string) : Promise<Song[]> {
     const token = await this.getAccessToken();
         // use the access token to access the Spotify Web API
-    const fieldsParams = 'fields=items(track(id%2C%20name%2C%20artists(name)))';
+    const fieldsParams = 'fields=items(track(preview_url%2C%20id%2C%20name%2C%20artists(name)))';
     const opts = {
       method: 'GET',
       url: 'https://api.spotify.com/v1/users/' + userId + '/playlists/'
@@ -41,6 +41,7 @@ export class SpotifyService {
                 artistName: item.track.artists[0].name,
                 id: item.track.id,
                 trackName: item.track.name,
+                previewUrl: item.track.preview_url,
               },
               );
       });
